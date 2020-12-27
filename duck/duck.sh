a3db574b04fecf280bddcfcfe1e4fba3ff0b59d3
Agustin Lavarello <agustinlare@gmail.com>
2020-12-27 12:45:45 -0300

    openvpn added

--- duck/duck.sh
+++ duck/duck.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Your comma-separated domains list
-DOMAINS="YOUR_DOMAINS"
+DOMAINS="agustinlare"
 # Your DuckDNS Token
-DUCKDNS_TOKEN="YOUR_DUCKDNS_TOKEN"
+DUCKDNS_TOKEN="f10fd620-97cc-426e-ae0f-a80ba1444305"
 curl -k -o /var/log/duck.log "https://www.duckdns.org/update?domains=${DOMAINS}&token=${DUCKDNS_TOKEN}&ip="
